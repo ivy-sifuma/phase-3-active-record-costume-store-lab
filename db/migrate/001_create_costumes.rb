c27b7b1d754efc0a9c40e2280f49bc4e0e4a540a
Ivy Sifuma <ivy.sifuma@student.moringaschool.com>
2022-11-08 12:22:49 +0300

    Completed

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -2,7 +2,7 @@ class CreateCostumes < ActiveRecord::Migration[6.1]
   def change
     create_table :costumes do |t|
       t.string :name
-      t.interger :price
+      t.float :price
       t.string :size 
       t.string :image_url
       t.datetime :created_at
